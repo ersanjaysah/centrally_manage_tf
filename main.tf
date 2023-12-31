5bea2be75ff36530bdbdbbe07d6a5584d188db89
sanjay sah <er.sanjaysah@hotmail.com>
2023-12-31 13:10:01 +0530

    centrally code manage

---
+++ main.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_security_group.mysg
+  id = "sg-0b04b50f5f157b922"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = "AKIATEPBZHAR3SW3Z67I"
+  secret_key = "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"
+}
